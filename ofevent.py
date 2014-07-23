ffa58a6a11ec75e93303415c4d8fa5b8c699cd27
Emmanuel OHAYON <emmanuel.ohayon@krono-safe.com>
2014-07-23 10:05:36 +0200

    WIP

--- ofevent.py
+++ ofevent.py
@@ -1,3 +1,11 @@
+import datetime
+
+################################################################################
+class ActionType:
+    MODIFY  = 0
+    CANCEL  = 1
+    CONFIRM = 2
+
 class OfEvent:
     def OfEvent(self, date_start, t_start, date_end, t_end, plane ):
         self._date_start = date_start
@@ -6,14 +14,28 @@ class OfEvent:
         self._t_end      = t_end
         self._plane      = plane
 
+    def __str__(self):
+        return "From " + str(date_start) + " " + str(t_start) + " to " + \
+               str(date_end) + " " + str(t_end) + " with " + str(plane)
+
 class OfAction:
     def OfAction(self, action, evt1, evt2=None):
-        assert action==ACTION_CANCEL or action==ACTION_MODIFY or \
-               action==ACTION_CONFIRM
-        assert evt2 is None or action==ACTION_MODIFY
+        assert action==ActionType.CANCEL or action==ActionType.MODIFY or \
+               action==ActionType.CONFIRM
+        assert evt2 is None or action==ActionType.MODIFY
         self._action = action
         self._evt1   = evt1
         self._evt2   = evt2
+
+    def __str__(self):
+        text = { ActionType.MODIFY  : "Modification" , \
+                 ActionType.CANCEL  : "Cancellation" , \
+                 ActionType.CONFIRM : "Confirmation" }
+
+        ans = text[_action] + " of:\n  " + str(_evt1)
+        if self._action==ACTION_MODIFY:
+            ans += "\nInto:\n  " + str(_evt2)
+        return ans
 
     def sync():
         # TODO
--- regexp.py
+++ regexp.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# vim: set fileencoding=utf-8:
 import re
 import ofevent
 
@@ -6,58 +7,60 @@ import ofevent
 
 REGEXP_DATE="\d{2}/\d{2}/\d{4}"
 REGEXP_TIME="\d{2}:\d{2}"
-REGEXP_PLANE="sur le (?P<plane>F-[A-Z]{4})"
 
-REGEXP_ACTION_CONFIRMED="est confirmée"
-REGEXP_ACTION_CANCELED="est annulée"
-REGEXP_ACTION_MODIFIED="est remplacée par"
+REGEXP_ACTION_CONFIRMED="est confirmÃ©e"
+REGEXP_ACTION_CANCELED="est annulÃ©e"
+REGEXP_ACTION_MODIFIED="est remplacÃ©e par"
 
 def regexp_resa(prefix=""):
-  return "La réservation du " + \
+  return "La rÃ©servation du " + \
             "(?P<" + prefix + "start_date>" + REGEXP_DATE + ") "    + \
             "(?P<" + prefix + "start_time>" + REGEXP_TIME + ") au " + \
             "(?P<" + prefix + "end_date>"   + REGEXP_DATE + ") "    + \
             "(?P<" + prefix + "end_time>"   + REGEXP_TIME + ")"
 
-REGEXP_MSG=regexp_resa() + "\s*" + REGEXP_PLANE + "\s*.*?" + \
+def regexp_plane(prefix=""):
+  return "sur le (?P<" + prefix + "plane>F-[A-Z]{4})"
+
+REGEXP_MSG=regexp_resa() + "\s*" + regexp_plane() + "\s*.*?" + \
            "(?P<action>" + REGEXP_ACTION_CONFIRMED + "|" + \
                            REGEXP_ACTION_CANCELED  + "|" + \
                            REGEXP_ACTION_MODIFIED +")"
 
 REGEXP_MODIF_MSG=\
-  regexp_resa("old_") + "\s*" + REGEXP_PLANE + "\s*.*?" + \
-  REGEXP_ACTION_MODIFIED + "\s*" + regexp_resa("new_")
-  # TODO plane a parametriser aussi...
+  regexp_resa("old_") + "\s*" + regexp_plane("old_") + "\s*.*?" + \
+  REGEXP_ACTION_MODIFIED + "\s*" + regexp_resa("new_") + "\s*" + \
+  regexp_plane("new_")
 
 
 
-SAMPLE_TEXT="""La réservation du 01/09/2014 18:30 au 01/09/2014 20:30
+SAMPLE_TEXT="""La rÃ©servation du 01/09/2014 18:30 au 01/09/2014 20:30
  sur le F-GGQO
 avec comme pilote OHAYON Emmanuel
 avec comme instructeur HUE Nicolas
- est confirmée.
-Cette opération a été effectuée par OHAYON Emmanuel
-La réservation du 07/09/2014 10:00 au 07/09/2014 13:00
+ est confirmÃ©e.
+Cette opÃ©ration a Ã©tÃ© effectuÃ©e par OHAYON Emmanuel
+La rÃ©servation du 07/09/2014 10:00 au 07/09/2014 13:00
  sur le F-GGQP
  avec comme commentaires : plage de silence
 avec comme pilote OHAYON Emmanuel
 avec comme instructeur HUE Nicolas
- est remplacée par La réservation du 07/09/2014 09:00 au 07/09/2014 12:00
+ est remplacÃ©e par La rÃ©servation du 07/09/2014 09:00 au 07/09/2014 12:00
  sur le F-GGQP
  avec comme commentaires : plage de silence
-La réservation du 27/07/2014 09:00 au 27/07/2014 12:00
+La rÃ©servation du 27/07/2014 09:00 au 27/07/2014 12:00
  sur le F-GGQP
 avec comme pilote OHAYON Emmanuel
 avec comme instructeur HUE Nicolas
- est annulée.
-Cette opération a été effectuée par HUE Nicolas
+ est annulÃ©e.
+Cette opÃ©ration a Ã©tÃ© effectuÃ©e par HUE Nicolas
 """
 
 ################################################################################
 
-ofevt_action = { REGEXP_ACTION_MODIFIED : ofevent.ACTION_MODIFY, \
-                 REGEXP_ACTION_CONFIRMED : ofevent.ACTION_CONFIRM, \
-                 REGEXP_ACTION_CANCELED : ofevent.ACTION_CANCEL }
+ofevt_action = { REGEXP_ACTION_MODIFIED  : ofevent.ActionType.MODIFY, \
+                 REGEXP_ACTION_CONFIRMED : ofevent.ActionType.CONFIRM, \
+                 REGEXP_ACTION_CANCELED  : ofevent.ActionType.CANCEL }
 
 def parse_text( text):
     actions = []
@@ -67,11 +70,11 @@ def parse_text( text):
 
     start=0
     while True:
-        msg = re.search( REGEXP_MSG, oneline_text[start:])
+        msg = re.search( REGEXP_MSG, oneline_text)
         if msg is None:
             break
 
-        evt1 = OfEvent( msg.group("start_date"), \
+        evt1 = ofevent.OfEvent( msg.group("start_date"), \
                         msg.group("start_time"), \
                         msg.group("end_time"),   \
                         msg.group("plane"))
@@ -79,14 +82,16 @@ def parse_text( text):
         action = ofevt_action[msg.group("action")]
 
         if msg.group("action")==REGEXP_ACTION_MODIFIED:
-            msg = re.search( REGEXP_MODIF_MSG, oneline_text[start:])
-            evt2 = OfEvent(msg.group("new_start_date"), \
+            msg = re.search( REGEXP_MODIF_MSG, oneline_text)
+            evt2 = ofevent.OfEvent(msg.group("new_start_date"), \
                            msg.group("new_start_time"), \
                            msg.group("new_end_time"),   \
                            msg.group("new_plane"))
 
-        actions.append( OfAction( action, evt1, evt2))
-        start += msg.end()
+        actions.append( ofevent.OfAction( action, evt1, evt2))
+        oneline_text = oneline_text[msg.end():]
 
     return actions
     
+for action in parse_text(SAMPLE_TEXT):
+    print(action)
